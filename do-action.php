c15c283c181aaf3c3ee43510b5420873262bfe19
david-binda <david.binovec@gmail.com>
2021-08-13 14:34:21 +0000

    Use better variable name
    s/filter_to_debug/action_to_debug/g

--- do-action.php
+++ do-action.php
@@ -2,18 +2,18 @@
 /**/
 class SWPD_do_action {
 
-	private $filter_to_debug = null;
+	private $action_to_debug = null;
 
 	private $callback = null;
 
 	public function __construct( $filter, $callback ) {
-		$this->filter_to_debug = $filter;
+		$this->action_to_debug = $filter;
 		$this->callback = $callback;
 		add_filter( 'all', array( $this, 'filter_all' ), 10, 2 );
 	}
 
 	public function filter_all( $action, $value = null ) {
-		if ( current_filter() === $this->filter_to_debug ) {
+		if ( current_filter() === $this->action_to_debug ) {
 			$this->add_debugging( $value );
 		}
 		return $value;
@@ -21,14 +21,14 @@ class SWPD_do_action {
 
 	public function add_debugging( $value ) {
 		global $wp_filter;
-		if ( true === array_key_exists( $this->filter_to_debug, $wp_filter ) ) {
-			$filters = $wp_filter[$this->filter_to_debug]->callbacks;
-			$wp_filter[$this->filter_to_debug]->callbacks = array();
+		if ( true === array_key_exists( $this->action_to_debug, $wp_filter ) ) {
+			$filters = $wp_filter[$this->action_to_debug]->callbacks;
+			$wp_filter[$this->action_to_debug]->callbacks = array();
 			$i = 0;
 			foreach( $filters as $priority => $thes_ ) {
 				foreach( $thes_ as $idx => $the_ ) {
-					$wp_filter[$this->filter_to_debug]->callbacks[$priority][$idx] = $the_;
-					$wp_filter[$this->filter_to_debug]->callbacks[$priority][ _wp_filter_build_unique_id( $this->filter_to_debug, array( $this, 'debug' ), $priority ) . $i++ ] = array(
+					$wp_filter[$this->action_to_debug]->callbacks[$priority][$idx] = $the_;
+					$wp_filter[$this->action_to_debug]->callbacks[$priority][ _wp_filter_build_unique_id( $this->action_to_debug, array( $this, 'debug' ), $priority ) . $i++ ] = array(
 						'function' => function( $value ) use ( $idx, $priority, $the_ ) { $this->debug( $value, $idx, $the_, $priority ); return $value; },
 						'accepted_args' => 1,
 					);
@@ -65,6 +65,6 @@ class SWPD_do_action {
 	}
 }
 
-function swpd_do_action_debug( $filter_to_debug, $callback ) {
-	new SWPD_do_action( $filter_to_debug, $callback );
+function swpd_do_action_debug( $action_to_debug, $callback ) {
+	new SWPD_do_action( $action_to_debug, $callback );
 }
